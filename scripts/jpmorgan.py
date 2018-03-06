fafdba0b08f2c5011f080a4c0b330461e9000ca4
pool23 <36885570+pool23@users.noreply.github.com>
2018-03-06 18:47:05 +0530

    Update jpmorgan.py

--- scripts/jpmorgan.py
+++ scripts/jpmorgan.py
@@ -10,9 +10,12 @@ import numpy as np
 import datetime
 import warnings
 from maks_lib import output_path
+from maks_lib import input_path
 warnings.simplefilter(action='ignore')
 
-df=pd.read_excel("JP Morgan Rate Sheet.xlsx")
+now = datetime.datetime.now()
+
+df=pd.read_excel(input_path+"JP Morgan Rate Sheet.xlsx")
 df1=df[8:14]
 df1.drop(df1.columns[[6]], axis=1, inplace=True)
 df2=df1[df1.columns[3:6]]
@@ -65,10 +68,7 @@ df7=df7[1:]
 frames = [df1,df2,df3,df4,df5,df7]
 result = pd.concat(frames)
 result["Bank Name"]="JP MORGAN"
-result.to_csv('Final JP MORGAN.csv')
-now = datetime.datetime.now()
 result["Date"]=now.strftime("%m/%d/%Y")
-result=result.reindex(columns=["Date","Consumer Deposit Rates","Interest","APY","Product","Terms(months)"])
 result= result.rename(columns={'Consumer Deposit Rates': 'Balance'})
 result=result.reindex(columns=["Date","Bank Name","Product","Balance","Interest","APY","Terms(months)"])
 result.to_csv(output_path + "JPM_Data_Deposit_{}.csv".format(now.strftime("%m_%d_%Y")), index=False )
