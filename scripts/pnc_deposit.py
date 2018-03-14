62b658081b45b67db28f0672220ba4fcc76a7b02
dsairam143 <37208282+dsairam143@users.noreply.github.com>
2018-03-14 15:17:34 +0530

    Bank product type updated

--- scripts/pnc_deposit.py
+++ scripts/pnc_deposit.py
@@ -313,6 +313,7 @@ if result is not None:
             bigdata = bigdata.append(mf, ignore_index=True)
             bigdata = bigdata.append(cdTableDF, ignore_index=True)
             bigdata = bigdata.append(racdf, ignore_index=True)
+            bigdata["Bank_Product_Type"] = "Savings"
             bigdata["Mortgage_Down_Payment"] = np.nan
             bigdata["Mortgage_Loan"] = np.nan
             bigdata["Min_Credit_Score_Mortagage"] = np.nan
