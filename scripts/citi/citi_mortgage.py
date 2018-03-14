0aff771caebb809d68f774bc6cab2344d5d29d65
deepak <deepakjon31@gmail.com>
2018-03-14 19:44:15 +0530

    Merge branch 'master' of C:\Users\deepak\PycharmProjects\pool with conflicts.

--- scripts/citi/citi_mortgage.py
+++ scripts/citi/citi_mortgage.py
@@ -7,7 +7,7 @@ import time
 import warnings
 import datetime
 import os
-from maks_lib import output_path
+
 
 now = datetime.datetime.now()
 
@@ -32,20 +32,29 @@ class Citi_mortgage:
     def fillform(self):
         time.sleep(6)
         self.driver.maximize_window()
+        #time.sleep(1)
         select = Select(self.driver.find_element_by_id("propertyKind"))
+        #time.sleep(1)
         select.select_by_visible_text("Single Family Home")
+        #time.sleep(1)
         select = Select(self.driver.find_element_by_id("propertyUse"))
         select.select_by_value("P")
+        #time.sleep(1)
         inutelement = self.driver.find_element_by_id("propCity")
         inutelement.send_keys('New York')
+        #time.sleep(1)
         select = Select(self.driver.find_element_by_id("propState"))
         select.select_by_value("NY")
+        #time.sleep(1)
         select = Select(self.driver.find_element_by_id("propCounty"))
         select.select_by_value("36061")
+        #time.sleep(1)
         inutelement = self.driver.find_element_by_id("purchPrice")
         inutelement.send_keys(self.eprice[1])
+        #time.sleep(1)
         inutelement = self.driver.find_element_by_id("desiredLoanAmount")
         inutelement.send_keys(self.dprice[1])
+        #time.sleep(1)
         self.driver.find_element_by_id("creditScoreGoodLabel").click()
         self.driver.find_element_by_id("submit-purchase").click()
         time.sleep(15)
@@ -78,90 +87,70 @@ class ExtractInfo(Citi_mortgage):
         for l in zip(h,d):
             la= l[1].split('\n')
             if 'points' in la[30]:
+                #f = l[0]+","+la[5]+","+la[12]+","+la[30]+'\n'
                 f = l[0] + "\t" + la[5] + "\t" + la[12]+'\n'
                 file.write(f)
 
             elif "points" in la[27]:
                 f = l[0]+"\t"+la[5]+"\t"+la[12]+'\n'
+                #f = l[0] + "," + la[5] + "," + la[12] + "," + la[27] + '\n'
                 file.write(f)
         file.close()
         time.sleep(3)
         dataset = pd.read_table('test.txt',sep='\t',delimiter=None, header=None)
         #dataset['Expected_Price'] = self.eprice[1]
         dataset['Mortgage_Loan'] = self.dprice
-        dataset["Product_Term"] = dataset.iloc[:,0].str.replace("Year Fixed","")
-        for i in range(0, len(dataset)):
-            if "Libor ARM" in dataset.ix[i]['Product_Term']:
-                dataset.ix[i]['Product_Term'] = 30 #dataset.iloc[i]['Product_Term']
+        dataset["Product_Term"] = dataset.iloc[:,0].str.replace("Fixed","")
 
         loan_type = pd.DataFrame(dataset.iloc[:,0].str.split(" ").tolist())
         dataset["Loan_Type"] = loan_type.iloc[:,2].str.replace("ARM","Variable")
+
         dataset.columns = ['Bank_Product_Name','Product_Interest','Mortgage_Apr',"Mortgage_Loan","Product_Term",'Loan_Type']
+        #print(dataset)
         dataset.to_csv("Citi_Mortgage_"+self.eprice[0]+".csv",index=False)
 
-if __name__ == "__main__":
-    print("Starting scraping")
 
+if __name__ == "__main__":
+    #log.info("Starting scrapping")
     expected_price = [('case1','120000'),('case2','360000'),('case3','600000')]
     desired_price = [('case1','100000'),('case2','300000'),('case3','500000')]
-    url = "https://online.citi.com/US/nccmi/purchase/ratequote/flow.action?fromLanding=true&selectedOption=CUSTOM&selectedOptionValue=CUSTOMpurChaseLanding&JFP_TOKEN=3GBHQLZG"
-    for i in range(len(expected_price)):
-        obj = Citi_mortgage(url, expected_price[i], desired_price[i])
-        obj.start_driver()
-        obj.get_url()
-        obj.fillform()
-        obj.save_page()
-        obj.close_driver()
-        time.sleep(3)
+    # url = "https://online.citi.com/US/nccmi/purchase/ratequote/flow.action?fromLanding=true&selectedOption=CUSTOM&selectedOptionValue=CUSTOMpurChaseLanding&JFP_TOKEN=3GBHQLZG"
+    # for i in range(len(expected_price)):
+    #     obj = Citi_mortgage(url, expected_price[i], desired_price[i])
+    #     obj.start_driver()
+    #     obj.get_url()
+    #     obj.fillform()
+    #     obj.save_page()
+    #     obj.close_driver()
+    #     time.sleep(5)
 
     tab1 = ['header-3 rate-card-panel-header-white', 'row rate-card-panel-items tb-hide']
     for i in range(len(expected_price)):
-        page = open("citi_mortgage_" + expected_price[i][0] + ".html", 'r')
-        extract = ExtractInfo(page, tab1, expected_price[i], desired_price[i][1])
+        extract = ExtractInfo(open("citi_mortgage_"+expected_price[i][0]+".html",'r'),tab1, expected_price[i], desired_price[i][1])
         extract.findtables_tab1()
-        page.close()
     df1 = pd.read_csv("Citi_Mortgage_case1.csv")
     df2 = pd.read_csv("Citi_Mortgage_case2.csv")
     df3 = pd.read_csv("Citi_Mortgage_case3.csv")
     df = pd.concat([df1, df2, df3])
-
     df['Date'] = now.strftime("%m-%d-%Y")
     df['Bank_Name'] = "CITIGROUP INC"
     df['Bank_Product'] = "Mortgage"
     df['Bank_Product_Type'] = "Mortgage"
-    df["Bank_Offer_Feature"] = "Offline"
+    df["Bank_Offer_Feature"] = "offline"
     df['Mortgage_Down_Payment'] = "20%"
-    df['Min_Credit_Score_Mortagage'] = "720+"
+    #print(df["Product_Term"].astype(str)[5])
+    for val, row in df.iterrows():
+        if "ARM" in row["Product_Term"]:
+            df.ix[val,"Product_Term"] = "30 Year"
+    dff = df.reindex(columns=["Date", "Bank_Name" , "Bank_Product" , 'Bank_Offer_Feature' ,
+                                    'Bank_Product_Type' , 'Bank_Product_Name' ,
+                                    'Mortgage_Loan' , 'Loan_Type' , 'Product_Term' , 'Mortgage_Down_Payment',
+                                    'Product_Interest' , 'Mortgage_Apr'])
 
-    #print(df['Product_Term'])
-
-    # for val, row in df.iterrows():
-    #     if "Libor ARM" in row["Product_Term"]:
-    #         #df.loc[val,'Product_Term'] = 30
-    #         #print(df.at[val,'Product_Term'])
-    #         pass
+    dff.to_csv("final_citi.csv")
+    os.remove("Citi_Mortgage_case1.csv")
+    os.remove("Citi_Mortgage_case2.csv")
+    os.remove("Citi_Mortgage_case3.csv")
 
 
-    #print(df['Product_Term'])
-    dff = df.reindex(columns=["Date", "Bank_Name" , "Bank_Product" ,
-                              'Bank_Product_Type', 'Bank_Offer_Feature' ,
-                              'Bank_Product_Name' , 'Product_Term' , 'Balance',
-                              'Product_Interest','Product_Apy',
-                              'Mortgage_Down_Payment','Mortgage_Loan',
-                              'Min_Credit_Score_Mortagage', 'Mortgage_Apr','Loan_Type'])
 
-    dff_consolidate = df.reindex(columns=["Date", "Bank_Name", "Bank_Product",
-                              'Bank_Product_Type', 'Bank_Offer_Feature',
-                              'Bank_Product_Name', 'Product_Term', 'Balance',
-                              'Product_Interest', 'Product_Apy',
-                              'Mortgage_Down_Payment', 'Mortgage_Loan',
-                              'Min_Credit_Score_Mortagage', 'Mortgage_Apr'])
-
-    dff.to_csv(output_path + "CITI_Data_Mortgage_{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
-    dff_consolidate.to_csv(output_path + "Consolidate_CITI_Data_Mortgage_{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
-
-    for rm in range(len(expected_price)):
-        os.remove("Citi_Mortgage_"+expected_price[rm][0]+".html")
-        os.remove("Citi_Mortgage_"+expected_price[rm][0]+".csv")
-
-    print("Finished Scraping ")
