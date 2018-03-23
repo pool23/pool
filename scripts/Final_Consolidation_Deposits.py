65fe94f3fdbf49cea1ed62a77aa2aff47cd36a71
pool23 <36885570+pool23@users.noreply.github.com>
2018-03-23 16:25:18 +0530

    Update Final_Consolidation_Deposits.py

--- scripts/Final_Consolidation_Deposits.py
+++ scripts/Final_Consolidation_Deposits.py
@@ -206,6 +206,6 @@ df_deposit['Maxm_Balance'] = df_deposit['Maxm_Balance'].str.replace("nan", "")
 
 # In[94]:
 
-
+df_deposit = df_deposit[((df_deposit.Bank_Product_Type == "CD") & (df_deposit.Term_in_Months.isin([6.0,12.0,36.0]))) |(df_deposit.Bank_Product_Type != "CD") ]
 df_deposit.to_csv(output_path+"US\\" + "US_Deposits_Data_{}.csv".format(now.strftime("%m_%d_%Y")), index=False )
 
