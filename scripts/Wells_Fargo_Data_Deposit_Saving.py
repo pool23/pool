8dd565691ab52147bacadfafda573aa9b897c475
pool23 <36885570+pool23@users.noreply.github.com>
2018-04-10 11:42:39 +0530

    Update Wells_Fargo_Data_Deposit_Saving.py

--- scripts/Wells_Fargo_Data_Deposit_Saving.py
+++ scripts/Wells_Fargo_Data_Deposit_Saving.py
@@ -80,34 +80,33 @@ if __name__ == '__main__':
     app.browser_close()
     df["Date"] = now.strftime("%m/%d/%Y")
     df["Bank Name"] = "Wells Fargo"
-    df = df.reindex(columns=["Date","Bank Name","Product Name","Term", "Balance", "Interest Rate", "APY"])
-    df.to_csv(output_path+ "WellsF_Data_Deposit_Saving.csv".format(now.strftime("%m_%d_%Y")), index=False)
+    df5 = df.reindex(columns=["Date","Bank Name","Product Name","Term", "Balance", "Interest Rate", "APY"])
+
 #####################################################################################################################
-df5 = pd.read_csv("C:\\Users\\Nimmi\\pool\\data\\output\\WellsF_Data_Deposit_Saving.csv")
-df5["Date"] = now.strftime("%m-%d-%Y")
-df5["Bank_Name"] = "WELLS FARGO"
-df5["Bank_Product"] = "Deposits"
-df5["Bank_Product_Type"] = "CD"
-df5["Bank_Offer_Feature"] = "Offline"
-df5["Bank_Product_Name"] = df5["Product Name"]
-df5["Product_Term"] = df5["Term"].str.strip("  months")
-df5["Balance"] = df5["Balance"]
-df5["Product_Interest"] = df5["Interest Rate"]
-df5["Product_Apy"] = df5["APY"]
-df5["Mortgage_Down_Payment"] = np.NAN
-df5["Mortgage_Loan"] = np.NAN
-df5["Min_Credit_Score_Mortagage"] = np.NAN
-df5["Mortgage_Apr"] = np.NAN
-df5 = df5.reindex(
-    columns=["Date", "Bank_Name", "Bank_Product", "Bank_Product_Type", "Bank_Offer_Feature", "Bank_Product_Name",
-             "Product_Term", "Balance", "Product_Interest", "Product_Apy", "Mortgage_Down_Payment", "Mortgage_Loan",
-             "Min_Credit_Score_Mortagage", "Mortgage_Apr"])
-df5.iloc[0:5]["Bank_Product_Type"]="Savings"
-df5.iloc[11:16]["Product_Term"]='3'
-df5.iloc[17:22]["Product_Term"]='6'
-df5.iloc[22:28]["Product_Term"]='12'
-df5 = df5.drop(df5.index[28])
-df6 = df5.iloc[4:,:]
-df5 = df5.iloc[0:1,:]
-df = pd.concat([df5, df6])
-df.to_csv(output_path + "Consolidate_WellsF_Data_Saving_Acc{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
+    df5["Date"] = now.strftime("%m-%d-%Y")
+    df5["Bank_Name"] = "WELLS FARGO"
+    df5["Bank_Product"] = "Deposits"
+    df5["Bank_Product_Type"] = "CD"
+    df5["Bank_Offer_Feature"] = "Offline"
+    df5["Bank_Product_Name"] = df5["Product Name"]
+    df5["Product_Term"] = df5["Term"].str.strip("  months")
+    df5["Balance"] = df5["Balance"]
+    df5["Product_Interest"] = df5["Interest Rate"]
+    df5["Product_Apy"] = df5["APY"]
+    df5["Mortgage_Down_Payment"] = np.NAN
+    df5["Mortgage_Loan"] = np.NAN
+    df5["Min_Credit_Score_Mortagage"] = np.NAN
+    df5["Mortgage_Apr"] = np.NAN
+    df5 = df5.reindex(
+        columns=["Date", "Bank_Name", "Bank_Product", "Bank_Product_Type", "Bank_Offer_Feature", "Bank_Product_Name",
+                 "Product_Term", "Balance", "Product_Interest", "Product_Apy", "Mortgage_Down_Payment", "Mortgage_Loan",
+                 "Min_Credit_Score_Mortagage", "Mortgage_Apr"])
+    df5.iloc[0:5]["Bank_Product_Type"]="Savings"
+    df5.iloc[11:16]["Product_Term"]='3'
+    df5.iloc[17:22]["Product_Term"]='6'
+    df5.iloc[22:28]["Product_Term"]='12'
+    df5 = df5.drop(df5.index[28])
+    df6 = df5.iloc[4:,:]
+    df5 = df5.iloc[0:1,:]
+    df = pd.concat([df5, df6])
+    df.to_csv(output_path + "Consolidate_WellsF_Data_Saving_Acc{}.csv".format(now.strftime("%m_%d_%Y")), index=False)
