5b0f6e8edae654aaa7bf004bb96bfc028cf540ae
vishal4python <36980240+vishal4python@users.noreply.github.com>
2018-08-10 14:19:57 +0530

    Update National_car_rental.py

--- Car_rental_scripts/National_car_rental.py
+++ Car_rental_scripts/National_car_rental.py
@@ -7,6 +7,7 @@ import pandas as pd
 from tabulate import tabulate
 import re
 import warnings
+from selenium.webdriver.common.keys import Keys
 warnings.simplefilter(action='ignore')
 
 
@@ -22,67 +23,76 @@ for i in airport:
     for k in [[15, 17], [15, 22]]:
         print(i[2])
 
+
+        start_date = (datetime.now() + timedelta(days=k[0])).strftime('%B %d')
+        start_day = str(int(start_date.split()[-1]))
+        start_month = str(start_date.split()[0])
+        end_date = (datetime.now() + timedelta(days=k[1])).strftime('%B %d')
+        end_day = str(int(end_date.split()[-1]))
+        end_month = str(end_date.split()[0])
+
+
+        print(start_date, end_date)
+        pickup_date = (datetime.now() + timedelta(days=k[0])).strftime('%m/%d/%Y')
+        return_date = (datetime.now() + timedelta(days=k[1])).strftime('%m/%d/%Y')
+        print('https://www.nationalcar.com/en_US/car-rental/reservation/startReservation.html')
+
+        browser.get('https://beta.nationalcar.com/en/car-rental.html?icid=ab-_-gotoBeta-_-startReservation&mboxSession=0eaa7efb533c477c867d509501fcf163&adobe_mc_ref=&adobe_mc_sdid=SDID%3D6272ABCE4665E897-5DDDAFA7C8B6B6A5%7CMCORGID%3D30545A0C536B768C0A490D44%2540AdobeOrg%7CTS%3D1533887357')
+        time.sleep(5)
+
+        browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').clear()
+        browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').send_keys(i[2])
+        time.sleep(5)
+        # browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').send_keys(Keys.ARROW_DOWN)
+        # browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').send_keys(Keys.RETURN)
+
+        browser.find_element_by_xpath('/html/body/div[2]/main/div[2]/section/div/div/div/h1').click()
+        # time.sleep(5)
+
+
+        browser.find_element_by_xpath('//*[@id="date-time__pickup-toggle"]').click()
+
+        browser.find_element_by_xpath("//*[@id='dateContainerId']//button[contains(@class, 'date-selector') and contains(@aria-label, '"+start_month+" "+start_day+"')]").click()
+
+        time.sleep(5)
+        browser.find_element_by_xpath('//*[@id="date-time__return-toggle"]').click()
+
+        browser.find_element_by_xpath("//*[@id='dateContainerId']//button[contains(@class, 'date-selector') and contains(@aria-label, '"+end_month+" "+end_day+"')]").click()
+
+        browser.find_element_by_css_selector('#booking-widget-inputs > div.booking-widget__right-column > div > button').click()
+        time.sleep(5)
         try:
-            start_date = (datetime.now() + timedelta(days=k[0])).strftime('%B %d')
-            end_date = (datetime.now() + timedelta(days=k[1])).strftime('%B %d')
-            print(start_date, end_date)
-            pickup_date = (datetime.now() + timedelta(days=k[0])).strftime('%m/%d/%Y')
-            return_date = (datetime.now() + timedelta(days=k[1])).strftime('%m/%d/%Y')
-            print('https://www.nationalcar.com/en_US/car-rental/reservation/startReservation.html')
 
-            browser.get('https://beta.nationalcar.com/en/car-rental.html?icid=ab-_-gotoBeta-_-startReservation&mboxSession=0eaa7efb533c477c867d509501fcf163&adobe_mc_ref=&adobe_mc_sdid=SDID%3D1E111004467E57F3-5ABD4C81FA00384D%7CMCORGID%3D30545A0C536B768C0A490D44%2540AdobeOrg%7CTS%3D1533021813')
-            time.sleep(5)
+            browser.find_element_by_xpath('/html/body/div[12]/div/div/div/div[2]/div/div[2]/div/button').click()
+        except:
+            pass
+        time.sleep(10)
+        jsoup = BeautifulSoup(browser.page_source)
+        car_details = jsoup.find('div',attrs={'class':'vehicle-list'})
+        for cars in car_details:
+            car_class = cars.find('div',attrs={'class':'vehicle__content'}).find('h2', attrs={'class':'vehicle__type-name'})
+            print(car_class.text)
+            car_name = cars.find('p', attrs={'class':'vehicle__type-desc'})
+            print(car_name.text)
+            per_day_later = cars.find('p', attrs={'class': 'vehicle__price-day'})
 
-            browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').clear()
-            browser.find_element_by_xpath('//*[@id="search-autocomplete__input-PICKUP"]').send_keys(i[2])
-            time.sleep(5)
-            browser.find_element_by_xpath('/html/body/div[2]/main/div[2]/section/div/div/div/h1').click()
-
-            time.sleep(5)
-
-
-            browser.find_element_by_xpath('//*[@id="date-time__pickup-toggle"]').click()
-
-            browser.find_element_by_xpath("//*[@id='dateContainerId']//button[contains(@class, 'date-selector') and contains(@aria-label, '"+str(start_date)+"')]").click()
-
-            time.sleep(5)
-            browser.find_element_by_xpath('//*[@id="date-time__return-toggle"]').click()
-
-            browser.find_element_by_xpath("//*[@id='dateContainerId']//button[contains(@class, 'date-selector') and contains(@aria-label, '"+str(end_date)+"')]").click()
-
-            browser.find_element_by_xpath('/html/body/div[2]/main/div[2]/section/div/div/div/form/div/div[2]/div/button').click()
-            time.sleep(5)
-            try:
-                browser.find_element_by_xpath('/html/body/div[10]/div/div/div/div[2]/div/div[2]/div/button').click()
-            except:
-                pass
-            time.sleep(10)
-            jsoup = BeautifulSoup(browser.page_source)
-            car_details = jsoup.find('div',attrs={'class':'vehicle-list'})
-            for cars in car_details:
-                car_class = cars.find('div',attrs={'class':'vehicle__content'}).find('h2', attrs={'class':'vehicle__type-name'})
-                print(car_class.text)
-                car_name = cars.find('p', attrs={'class':'vehicle__type-desc'})
-                print(car_name.text)
-                per_day_later = cars.find('p', attrs={'class': 'vehicle__price-day'})
-
-                per_day_later = re.search('(\$[0-9\., ]+)(/[a-zA-Z ]+)', per_day_later.text) if per_day_later is not None else None
-                per_day_later, per_day_later_unit = (per_day_later.group(1),per_day_later.group(2).replace('/','')) if per_day_later is not None else (None, None)
-                print(per_day_later)
-                print(per_day_later_unit)
+            per_day_later = re.search('(\$[0-9\., ]+)(/[a-zA-Z ]+)', per_day_later.text) if per_day_later is not None else None
+            per_day_later, per_day_later_unit = (per_day_later.group(1),per_day_later.group(2).replace('/','')) if per_day_later is not None else (None, None)
+            print(per_day_later)
+            print(per_day_later_unit)
 
 
-                pay_later_total = cars.find('p', attrs={'class': 'vehicle__price-total'})
+            pay_later_total = cars.find('p', attrs={'class': 'vehicle__price-total'})
 
-                pay_later_total = re.search('(\$[0-9\., ]+)([a-zA-Z ]+)', pay_later_total.text) if pay_later_total is not None else None
-                pay_later_total,pay_later_total_unit = (pay_later_total.group(1),pay_later_total.group(2)) if pay_later_total is not None else (None,None)
-                print(pay_later_total)
-                print(pay_later_total_unit)
-                data = [datetime.now().strftime('%m/%d/%Y'), pickup_date, return_date, i[1], i[2], i[2], i[1],car_class.text,
-                        car_name.text, None, None, None, None, per_day_later,per_day_later_unit,pay_later_total, pay_later_total_unit,'National']
-                car_data.append(data)
-        except Exception as e:
-            print(e)
+            pay_later_total = re.search('(\$[0-9\., ]+)([a-zA-Z ]+)', pay_later_total.text) if pay_later_total is not None else None
+            pay_later_total,pay_later_total_unit = (pay_later_total.group(1),pay_later_total.group(2)) if pay_later_total is not None else (None,None)
+            print(pay_later_total)
+            print(pay_later_total_unit)
+            data = [datetime.now().strftime('%m/%d/%Y'), pickup_date, return_date, i[1], i[2], i[2], i[1],car_class.text,
+                    car_name.text, None, None, None, None, per_day_later,per_day_later_unit,pay_later_total, pay_later_total_unit,'National']
+            car_data.append(data)
+        # except Exception as e:
+        #     print(e)
 
 
 
